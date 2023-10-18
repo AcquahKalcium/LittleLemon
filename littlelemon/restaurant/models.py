b0f6b03f8323a5be7260e4d46458f42b9b5a2c85
Acquahkalcium <kalcium3@gmail.com>
2023-10-17 22:31:09 -1200

    models added

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guest = models.IntegerField()
+    booking_date = models.DateField()
+
+    def __str__(self): 
+        return self.name
+
+
+# Add code to create Menu model
+class Menu(models.Model):
+   id = models.IntegerField(primary_key=True)
+   title =  models.CharField(max_length=255)
+   price = models.DecimalField(max_digits=10, decimal_places=2) 
+   inventory = models.IntegerField() 
+
+   def __str__(self): 
+        return self.title
